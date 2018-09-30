76a686d75a1de71b8f7b7fc18c76ff85537aef19
trungnam99 <trungnam23799@gmail.com>
2018-09-30 15:05:36 +0700

    1

---
+++ vector.cpp
@@ -0,0 +1,46 @@
+
+33#include<iostream>
+using namespace std;
+struct vector
+{
+	double dai, rong, cao;
+};
+vector operator +(vector X, vector Y)// operator da nang hoa toan tu
+{
+	vector Z;
+	Z.dai = X.dai + Y.dai;
+	Z.rong = X.rong + Y.rong;
+	Z.cao = X.cao + Y.cao;
+	return Z;
+}
+vector operator -(vector X, vector Y)
+{
+	vector Z;
+	Z.dai = X.dai - Y.dai;
+	Z.rong = X.rong - Y.rong;
+	Z.cao = X.cao - Y.cao;
+	return Z;
+}
+double operator *(vector X, vector Y)
+{
+	return X.dai*Y.dai + X.rong*Y.rong + X.cao*Y.cao;
+}
+istream &operator>>(istream &in, vector &X)
+{
+	cout << "dai= "; in >> X.dai;
+	cout << "rong= "; in >> X.rong;
+	cout << "cao= "; in >> X.cao;
+	return in;
+}
+ostream &operator<<(ostream &out, vector X)
+{
+	out << "(" << X.dai <<"," << X.rong << "," << X.cao << ")";
+	return out;
+}
+int main()
+{
+	vector X, Y, Z;
+	cin >> X>>Y;
+	cout << X + Y << endl << X - Y << endl<<X*Y  << endl;
+}
+
